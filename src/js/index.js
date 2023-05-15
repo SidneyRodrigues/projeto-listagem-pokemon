ab1cf7868dc95e524af765f4ce7ecd8ca0982aeb
Sidney Rodrigues <104949601+SidneyRodrigues@users.noreply.github.com>
2023-05-14 23:54:17 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+
+
+const botaoAlterarTema = document.getElementById("botao-alterar-tema");
+const body = document.querySelector("body");
+const imagemBotaoTrocaDeTema = document.querySelector(".imagem-botao");
+
+botaoAlterarTema.addEventListener("click", () => {
+  const modoEscuroEstaATivo = body.classList.contains("modo-escuro");
+
+  body.classList.toggle('modo-escuro')
+
+  if (modoEscuroEstaATivo) {
+    imagemBotaoTrocaDeTema.setAttribute("src", "./src/images/sun.png");
+  } else {
+    imagemBotaoTrocaDeTema.setAttribute("src", "./src/images/moon.png");
+  }
+});
